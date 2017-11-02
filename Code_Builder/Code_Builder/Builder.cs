5f08912694617e3882b6a2b2f40cbb27cd549006
Gustavo <tavo951073@gmail.com>
2017-11-02 17:19:21 -0500

    Codigo del proyecto Builder

---
+++ Code_Builder/Code_Builder/Builder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Builder
+{
+    /// <summary>
+    /// The 'Builder' abstract class
+    /// </summary>
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract Product GetResult();
+    }
+}
---
+++ Code_Builder/Code_Builder/Director.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Builder
+{
+    /// <summary>
+    /// The 'Director' class
+    /// </summary>
+    class Director
+    {
+        // Builder uses a complex series of steps
+        public void Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+}
